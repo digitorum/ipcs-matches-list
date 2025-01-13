8c84cd46e55914715b315afaa8ebff10043738ff
digitorum <pavel.ladygin@gmail.com>
2025-01-13 23:51:15 +0300

    Причесана таблица со списком матчей

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -9,28 +9,32 @@
     }">
   
     <template #date-data="{ row }">
-      {{ row.dateDiapason }}
+      {{ row.startDate }}<span
+        v-if="row.endDate"
+        class="hidden lg:inline"> - {{ row.endDate }}</span>
     </template>
 
     <template #match-data="{ row }">
       <div
-        class="flex">
+        class="flex flex-col lg:flex-row">
         <div
-          class="grow">
+          class="grow mr-2">
           <ULink
             :to="row.url"
             target="_blank">
             {{ row.name }}
           </ULink>
         </div>
-        <div>
+        <div
+          v-if="row.level || row.exercisesCount || row.minimumShots"
+          class="whitespace-nowrap mt-2 lg:mt-0">
           <UTooltip
             v-if="row.level"
             text="Уровень">
             <UBadge
               :icon="`i-tabler-circle-number-${row.level}`"
               :ui="{ rounded: 'rounded-full' }"
-              class="ml-2"
+              class="mr-2"
               color="primary"
               size="xs"
               variant="soft" />
@@ -40,7 +44,7 @@
             text="Количество упражнений">
             <UBadge
               :ui="{ rounded: 'rounded-full' }"
-              class="ml-2"
+              class="mr-2"
               color="green"
               icon="i-tabler-file"
               size="xs"
@@ -53,7 +57,7 @@
             text="Количество выстрелов (минимум)">
             <UBadge
               :ui="{ rounded: 'rounded-full' }"
-              class="ml-2"
+              class="mr-2"
               color="indigo"
               icon="i-tabler-sum"
               size="xs"
@@ -66,15 +70,10 @@
     </template>
 
     <template #price-data="{ row }">
-      <template v-if="row.rawPrice">
-        <UTooltip
-          :text="row.rawPrice">
-          {{ row.price }}
-        </UTooltip>
-      </template>
-      <template v-else>
-        {{ row.price }}
-      </template>
+      <UTooltip
+        :text="row.price">
+        <span class="truncate overflow-hidden max-w-24">{{ row.price }}</span>
+      </UTooltip>
     </template>
 
     <template #city-data="{ row }">
@@ -93,34 +92,45 @@
 </script>
 
 <script setup lang="ts">
-  
-import { cutspace } from '../lib/string';
-
 const { data } = await useFetch<Site.Api.IMatchesListResponse>('/api/matches-list')
 
 const columns =  [
-  { label: 'Дата', key: 'date', sortable: true },
-  { label: 'Мероприятие', key: 'match', sortable: false },
-  { label: 'Cтоимость', key: 'price', sortable: false },
-  { label: 'Место проведения', key: 'city', sortable: true}
+  {
+    label: 'Дата',
+    key: 'date',
+    sortable: true,
+    class: 'min-w-24 lg:min-w-48'
+  },
+  {
+    label: 'Мероприятие',
+    key: 'match',
+    sortable: false,
+    class: 'min-w-72'
+  },
+  {
+    label: 'Cтоимость',
+    key: 'price',
+    sortable: false
+  },
+  {
+    label: 'Место',
+    key: 'city',
+    sortable: true
+  }
 ]
 
 const matches = computed(() => {
   return data.value?.matches.map((match) => {
-    const price = cutspace(match.price, 20)
-
     return {
       date: new Date(match.startDate).getTime(), // поле для сортировки
-      dateDiapason: getDate(match.startDate) + (match.endDate ? ` - ${getDate(match.endDate)}` : ''),
+      startDate: getDate(match.startDate),
+      endDate: match.endDate ? getDate(match.endDate) : '',
       level: match.level,
       name: match.name,
       url: match.url,
       exercisesCount: match.exercisesCount,
       minimumShots: match.minimumShots,
-      rawPrice: match.price === price 
-        ? null
-        : match.price,
-      price,
+      price: match.price,
       city: match.location.city?.name ?? '...',
       location: match.location.description
     }
